a7c7cf7364e34ab984ed2112cd2829a688f75b1f
TheHandOfKing <a.marjanovic26@gmail.com>
2021-11-20 17:47:00 +0100

    my acc

--- app/Controllers/Products.php
+++ app/Controllers/Products.php
@@ -34,6 +34,23 @@ class Products extends Controller {
     return $response;
   }
 
+  public function getProducts() {
+      $args = array(
+        'post_type'      => 'product',
+        'posts_per_page' => 10,
+        'product_cat'    => 'hoodies'
+    );
+
+    $loop = new WP_Query( $args );
+
+    while ( $loop->have_posts() ) : $loop->the_post();
+        global $product;
+        echo '<br /><a href="'.get_permalink().'">' . woocommerce_get_product_thumbnail().' '.get_the_title().'</a>';
+    endwhile;
+
+    wp_reset_query();
+  }
+
   // Shorten Product title if its too long
   public function getExcerptForProductTitle($title) {
     $excerpt = $title;
