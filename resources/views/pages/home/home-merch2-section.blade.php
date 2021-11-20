273eabee230b4a26a4f613d39a5ca5a6adfedc04
unknown <Dev@alphaefficiency.com>
2021-11-20 19:46:26 +0100

    Sections added

---
+++ resources/views/pages/home/home-merch2-section.blade.php
@@ -0,0 +1,12 @@
+<section class="home-merch2-section">
+  <div class="content">
+    <h2>Saradnja sa drugim brendovima</h2>
+    <p>
+      Majice su napravljene od najfinijeg pamuka sa malom kolicinom elastina,
+      prijatne su za nosenje bla bla bla bla bla
+    </p>
+    <a href="">
+      Pogledaj sve
+    </a>
+  </div>
+</section>
--- resources/views/template-home.blade.php
+++ resources/views/template-home.blade.php
@@ -7,5 +7,7 @@
 @section('content')
   @while(have_posts()) @php the_post() @endphp
     @include('pages.home.home-hero-section')
+    @include('pages.home.home-merch1-section')
+    @include('pages.home.home-merch2-section')
   @endwhile
 @endsection
