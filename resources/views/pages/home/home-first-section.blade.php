883de4d321ce5ae4b965c5d216ee53721e8a9326
TheHandOfKing <a.marjanovic26@gmail.com>
2022-04-04 15:12:31 +0200

    prva sekcija

---
+++ resources/views/pages/home/home-first-section.blade.php
@@ -0,0 +1,31 @@
+<section class="home-first-section">
+  <div class="wrap d-flex container">
+    <div class="left d-flex">
+      <div class="left-numbers">
+        <div class="line-holder">
+          <span class="main-line"></span>
+          <span class="followup-line"></span>
+        </div>
+
+        <div class="numbers">
+          <span class="number active">01</span>
+          <span class="number">02</span>
+          <span class="number">03</span>
+        </div>
+      </div>
+      <div class="right-desc">
+        <h1>-Just <br>Prostorija</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+          ex ea commodo consequat.</p>
+  
+        <a href="#">Check It</a>
+      </div>
+    </div>
+    <div class="right">
+      <div class="image">
+        <img src="@asset('images/homepage/first-section-image.png')" alt="videoplayback">
+      </div>
+    </div>
+  </div>
+</section>
