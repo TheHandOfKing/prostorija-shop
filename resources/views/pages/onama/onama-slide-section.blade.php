7d67ba6ce19351ff74e1a86b0a56c4d1baa92f06
unknown <Dev@alphaefficiency.com>
2022-08-03 23:56:14 +0200

    onama completed

--- resources/views/pages/onama/onama-slide-section.blade.php
+++ resources/views/pages/onama/onama-slide-section.blade.php
@@ -1,3 +1,48 @@
 <section class="onama-slide-section">
-  slide
+  <div id="carouselExampleControls" class="carousel slide carousel-fade" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="@asset('images/onama/slide1.png')" alt="O nama">
+        <div class="text">
+          <h3>Slide1</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat.
+          </p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="@asset('images/onama/slide1.png')" alt="O nama">
+        <div class="text">
+          <h3>Slide2</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat.
+          </p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="@asset('images/onama/slide1.png')" alt="O nama">
+        <div class="text">
+          <h3>Slide3</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat.
+          </p>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+  <img class="arrow-holder" src="@asset('images/onama/arrow-holder.png')" alt="Background">
 </section>
