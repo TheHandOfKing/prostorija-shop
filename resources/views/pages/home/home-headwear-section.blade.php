391390176d84545fb20018c51c1b374f21d8de8c
TheHandOfKing <a.marjanovic26@gmail.com>
2022-08-23 18:14:17 +0200

    headwear section changes

---
+++ resources/views/pages/home/home-headwear-section.blade.php
@@ -0,0 +1,13 @@
+<section class="home-headwear-section">
+    <div class="container d-flex">
+        <div class="left">
+            <h1>Headwear</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione magnam animi blanditiis cumque ab ad cum sunt reiciendis. Sequi vero modi voluptates, veniam exercitationem itaque? Dolores id vero sed impedit?</p>
+            <button>Check It</button>
+        </div>
+
+        <div class="right">
+            <img src="@asset('images/homepage/headwear.png')" class="image-headwear" alt="headwear">
+        </div>
+    </div>
+</section>
