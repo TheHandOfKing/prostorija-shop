1600b9ce2a8993aea64c3a822af39f31eb8b5e7a
Ilija <ilijatoskovic@gmail.com>
2022-11-22 03:00:11 +0100

    headwear section responsive

--- resources/views/template-home.blade.php
+++ resources/views/template-home.blade.php
@@ -6,8 +6,8 @@
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
-    {{-- @include('pages.home.home-first-section')
-    @include('pages.home.home-headwear-section') --}}
+    {{-- @include('pages.home.home-first-section') --}}
+    @include('pages.home.home-headwear-section')
     @include('pages.home.home-slider-section')
     @include('pages.home.home-about-section')
   @endwhile
