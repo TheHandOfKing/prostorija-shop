4abd872c3b58ddc3bc5382b269fbe8fddf10f85a
Ilija <ilijatoskovic@gmail.com>
2022-11-26 01:39:37 +0100

    search page completed

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,18 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
+@include('partials.page-header')
 
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, no results were found.', 'sage') }}
-    </div>
-    {!! get_search_form(false) !!}
-  @endif
-
-  @while(have_posts()) @php the_post() @endphp
+@if (!have_posts())
+<div class="alert-holder">
+  <div class="alert alert-warning">
+    {{ __('Sorry, no results were found.', 'sage') }}
+  </div>
+</div>
+@endif
+<div class="content">
+  <div class="search-articles-holder">
+    @while(have_posts()) @php the_post() @endphp
     @include('partials.content-search')
-  @endwhile
-
-  {!! get_the_posts_navigation() !!}
+    @endwhile
+  </div>
+</div>
+{!! get_the_posts_navigation() !!}
 @endsection
